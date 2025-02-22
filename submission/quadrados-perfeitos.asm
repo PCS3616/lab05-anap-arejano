100cc789c50fca018d6e16a2c9c4cdffc6f6e5fb
pcs3616 <aparejano@gmail.com>
2025-02-21 22:33:06 -0300

    erro da memoria corrigido

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -5,19 +5,19 @@
     LOOP LD N ; carrega N
     SB MEIA_TRES ; subtrai 63 de N
     JZ FIM_SUBROTINA ; jump se for zero 
-    LD DOIS; carrega 2 no ac
-    ML POSICAO_MEMORIA ; multiplica a ultima posicao da memoria por 2
+    LD POSICAO_MEMORIA; carrega a ultima posicao da memoria no ac
+    ML DOIS ; multiplica a ac(com a ultima posicao da memoria) por 2
     AD UN ; soma 1 no ac
     MM LEMBRA_RESULTADO ; guarda provisoriamente o resultado
-    LD POSICAO_MEMORIA ;
-    AD DOIS;
-    MM POSICAO_MEMORIA ;
-    LD LEMBRA_RESULTADO;
-    MM POSICAO_MEMORIA;
-    LD N ;
-    AD UN ;
-    MM N ;
-    JP LOOP;
+    LD POSICAO_MEMORIA ; carrega posicao no ac
+    AD DOIS; soam 2 no ac  
+    MM POSICAO_MEMORIA ; guarda resultado na posicao
+    LD LEMBRA_RESULTADO; carrega o resultado anterior
+    MM POSICAO_MEMORIA; guarda o resultado anterior na nova posiçao de memoria
+    LD N ; carrega n
+    AD UN ; soma um no ac
+    MM N ; guarda o novo valor de n 
+    JP LOOP; retorna pro loop 
 
     FIM_SUBROTINA RS QUADRADOS
 
